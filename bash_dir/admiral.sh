6fcc3ac579a5dcf7d180386b7c4e823ba7206a34
Timur <timurka.zavyalov@mail.ru>
2022-02-24 13:13:08 +0300

    innit_three

--- bash_dir/admiral.sh
+++ bash_dir/admiral.sh
@@ -29,25 +29,3 @@ do
     mv "${file}" "${done_path}"
     mv "${csv_name}" "${done_path}"
 done
-
-#xls_path=/home/timur/PycharmWork/PORT_LINE_CSV/test
-#path="${xls_path}"/* | tr A-Z a-z
-#echo "${path}"
-#for file in "${xls_path}/"*.csv | tr A-Z a-z;
-#do
-#    echo "${file}"
-#done
-
-
-#files_count="$(ls -l $xls_list | wc -l)"
-#echo "Will work on ${files_count} in folder ${xls_path}"
-#for xls in ${xls_list}
-#do
-#  echo "${xls}"
-##	csv_name="${xls_path}/csv/$(basename "$xls").csv"
-##  	echo "Will convert Excel $xls to CSV ${csv_name}"
-##  	in2csv "$xls" > "$csv_name"
-##  	mv "$xls" "${xls_path}/excel_pushed_to_csv"
-##
-##  	/bin/sh "${xls_path}/csv2json.sh" "$csv_name"
-#done
