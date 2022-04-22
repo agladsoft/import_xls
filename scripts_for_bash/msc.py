0b47cac8500de59fb778aa8152e6c0614e9c11e2
Timur <timurka.zavyalov@mail.ru>
2022-04-22 17:09:30 +0300

    fix: added duplicate containers and removed a space between numbers

--- scripts_for_bash/msc.py
+++ scripts_for_bash/msc.py
@@ -24,9 +24,17 @@ def isDigit(x):
     try:
         x = re.sub('(?<=\d) (?=\d)', '', x)
         float(x)
-        return True
+        return True, float(x)
     except ValueError:
-        return False
+        return False, x
+
+
+# def isDigit2(x):
+#     try:
+#         x = re.sub('(?<=\d) (?=\d)', '', x)
+#         return x
+#     except ValueError:
+#         pass
 
 
 def max_numbers(s):
@@ -34,28 +42,52 @@ def max_numbers(s):
 
 
 def add_data_to_parced(parsed_data, line, context, num1, num2):
-    logging.info(u"Checking if we are on common line with number...")
-    range_id = line[:2]
-    match_id = [isDigit(id) for id in range_id]
-    add_id = match_id.index(True)
-    line_id = str(range_id[add_id])
-    if isDigit(line_id):
-        logging.info(u"Ok, line looks common...")
-        parsed_record = {'container_number': line[add_id + 1].strip()}
-        container_size_and_type = re.findall("\w{1,2}", line[add_id + 2].strip())
-        parsed_record['container_size'] = int(float(container_size_and_type[0]))
-        parsed_record['container_type'] = container_size_and_type[1]
-        parsed_record['goods_weight'] = max_numbers(line[add_id + 7].replace(' ', '')) if line[add_id + 7] else None
-        parsed_record['package_number'] = int(float(line[add_id + 5])) if line[add_id + 5] else None
-        parsed_record['goods_name_rus'] = line[add_id + 4].strip()
-        parsed_record['consignment'] = line[add_id + 8].strip()
-        parsed_record['shipper'] = line[add_id + num1].strip()
-        parsed_record['shipper_country'] = line[add_id + num2].strip()
-        parsed_record['consignee'] = line[add_id + 9].strip()
-        parsed_record['city'] = line[add_id + 10].strip()
-        record = merge_two_dicts(context, parsed_record)
-        logging.info(u"record is {}".format(record))
-        parsed_data.append(record)
+    global add_id_2
+    try:
+        logging.info(u"Checking if we are on common line with number...")
+        range_id = line[:2]
+        match_id = [isDigit(id)[0] for id in range_id]
+        add_id = match_id.index(True)
+        add_id_2 = add_id
+        line_id = str(range_id[add_id])
+        if isDigit(line_id)[0]:
+            logging.info(u"Ok, line looks common...")
+            parsed_record = {'container_number': line[add_id + 1].strip()}
+            container_size_and_type = re.findall("\w{1,2}", line[add_id + 2].strip())
+            parsed_record['container_size'] = int(float(container_size_and_type[0]))
+            parsed_record['container_type'] = container_size_and_type[1]
+            parsed_record['goods_weight'] = max_numbers(line[add_id + 7].replace(' ', '')) if line[add_id + 7] else None
+            parsed_record['package_number'] = int(isDigit(line[add_id + 5])[1]) if line[add_id + 5] else None
+            parsed_record['goods_name_rus'] = line[add_id + 4].strip()
+            parsed_record['consignment'] = line[add_id + 8].strip()
+            parsed_record['shipper'] = line[add_id + num1].strip()
+            parsed_record['shipper_country'] = line[add_id + num2].strip()
+            parsed_record['consignee'] = line[add_id + 9].strip()
+            parsed_record['city'] = line[add_id + 10].strip()
+            record = merge_two_dicts(context, parsed_record)
+            logging.info(u"record is {}".format(record))
+            parsed_data.append(record)
+    except ValueError as ex:
+        if line[add_id_2 + 1] and line[add_id_2 + 2] and line[add_id_2 + 3] and line[add_id_2 + 4] and line[add_id_2
+                                                                                                            + 5]:
+            logging.info(u"Ok, line looks common...")
+            parsed_record = {'container_number': line[add_id_2 + 1].strip()}
+            container_size_and_type = re.findall("\w{1,2}", line[add_id_2 + 2].strip())
+            parsed_record['container_size'] = int(float(container_size_and_type[0]))
+            parsed_record['container_type'] = container_size_and_type[1]
+            parsed_record['goods_weight'] = max_numbers(line[add_id_2 + 7].replace(' ', '')) if line[add_id_2 + 7] \
+                else \
+                None
+            parsed_record['package_number'] = int(isDigit(line[add_id_2 + 5])[1]) if line[add_id_2 + 5] else None
+            parsed_record['goods_name_rus'] = line[add_id_2 + 4].strip()
+            parsed_record['consignment'] = line[add_id_2 + 8].strip()
+            parsed_record['shipper'] = line[add_id_2 + num1].strip()
+            parsed_record['shipper_country'] = line[add_id_2 + num2].strip()
+            parsed_record['consignee'] = line[add_id_2 + 9].strip()
+            parsed_record['city'] = line[add_id_2 + 10].strip()
+            record = merge_two_dicts(context, parsed_record)
+            logging.info(u"record is {}".format(record))
+            parsed_data.append(record)
 
 
 class OoclCsv(object):
@@ -65,7 +97,7 @@ class OoclCsv(object):
 
     def process(self, input_file_path):
         context = dict(line=os.path.basename(__file__).replace(".py", ""))
-        context['terminal'] = os.environ.get('XL_IMPORT_TERMINAL')
+        context['terminal'] = 'nle'
         date_previous = re.match('\d{2,4}.\d{1,2}', os.path.basename(input_file_path))
         date_previous = date_previous.group() + '.01' if date_previous else date_previous
         if date_previous is None:
@@ -124,6 +156,8 @@ class OoclCsv(object):
         return parsed_data
 
 
+# input_file_path = '/home/timur/Anton_project/import_xls-master/MSC_НУТЭП_ДЕК.2020/csv/2022.01 Разнарядка MED AYDIN AO202A .xls.csv'
+# output_folder = '/home/timur/Anton_project/import_xls-master/MSC_НУТЭП_ДЕК.2020/json'
 input_file_path = os.path.abspath(sys.argv[1])
 output_folder = sys.argv[2]
 basename = os.path.basename(input_file_path)
